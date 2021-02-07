3da00e0ae4ddfc92c1efba4d9e70e98fd3a7641b
eilla1 <exu6056@gmail.com>
2021-02-07 00:09:24 -0800

    testing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello, World!
+          Hello, World.
         </p>
       </header>
     </div>
