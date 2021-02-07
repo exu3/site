88c9575c556a2568d854d239636e279af5665d87
eilla1 <exu6056@gmail.com>
2021-02-07 13:14:04 -0800

    :tada: add card to page

--- src/index.js
+++ src/index.js
@@ -3,19 +3,10 @@ import './components/main.scss';
 import ReactDOM from 'react-dom';
 import { Accordion, Card } from './components'
 
-
 ReactDOM.render(
   <div>
     <Accordion />
+    <Card />
   </div>,
 	document.getElementById('root')
 );
-
-// export default function Home () {
-//   return (
-//     <div>
-//       <Accordion />
-//       <Card />
-//     </div>
-//   );
-// }
