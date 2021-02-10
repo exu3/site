52e1fcb1eec78b8e00a38f718e4bc00af55070f7
eilla1 <exu6056@gmail.com>
2021-02-10 00:26:19 -0800

    :tada: create card component
    to do: fit image in container, sass mixins for resposniveness

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import "./Card.scss";
 
-const Card = () => {
+class Card extends React.Component {
+  render() {
+    const { blurb, projectName, metaInfo, image, alt } = this.props;
     return (
-        <p>this is cool card</p>
-    )
+      <div className="column">
+        <div className="card">
+          <div className="card-header">
+            <figure className="thumbnail">
+              <img src={image} alt={alt} />
+            </figure>
+            <p className="card-title">{projectName}</p>
+            <p className="card-meta">{metaInfo}</p>
+          </div>
+          <div className="card-body">{blurb}</div>
+          <div className="card-footer">
+            <button className="btn"> icon </button>
+            <button className="btn"> icon </button>
+            <button className="btn"> icon </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Card
+export default Card;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Accordion, Card, Header } from './components'
 const homepage = (
   <div>
     <Header />
-    <Card />
+    <Card blurb="this is a blurb" projectName="Project Name" metaInfo="react.js figma" image="https://www.pngkit.com/png/detail/637-6371563_facebook-small-icon-png.png" alt="randompic" />
     <Accordion />
   </div>
 );
