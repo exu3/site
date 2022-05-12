7a1fb865b6924569eece7ed882b12a4d8960c459
exu <git@ella.cx>
2022-05-12 15:42:43 -0700

    Fix dark mode text

--- components/guestbook/Entry.tsx
+++ components/guestbook/Entry.tsx
@@ -3,7 +3,7 @@ import { formatDate } from "../../lib/date";
 export default function Entry({ name, message, createdAt }): JSX.Element {
   return (
     <div>
-      <p className="text-sm text-gray-500">
+      <p className="text-sm text-gray-500 dark:text-gray-300">
         <span>{name}</span> on <span>{formatDate(createdAt)}</span>
       </p>
       <pre>{message}</pre>
