fb3192749465e726347249616bcb769aca518e00
exu <git@ella.cx>
2022-05-12 19:10:46 -0700

    Initial work on sup page

---
+++ pages/sup.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const SupPage = () => {
+  return (
+    <Layout heading="Here's what's up:">
+      <p>hi</p>
+    </Layout>
+  );
+};
+
+export default SupPage;
