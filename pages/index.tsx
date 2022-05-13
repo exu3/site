1350c22fa30d6d783d86ce7cfe85e45e1602d356
exu <git@ella.cx>
2022-05-12 19:00:19 -0700

    Hello to the humans and bots

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,28 @@ import Layout from "../components/Layout";
 import Link from "next/link";
 import Image from "next/image";
 import cat from "../public/images/cat.jpg";
+import { useState } from "react";
 
 const HomePage = () => {
   const time = new Date();
   const greeting = time.getHours() < 12 ? "Good morning ☀️" : "Good day 👋";
 
+  const [hello, setHello] = useState("Hello.");
+  const hellos = ["Hello.", "Bonjour.", "Hola.", "Hallo.", "你好。"];
   return (
     <>
       <Layout heading={greeting} showNav={false}>
         <article>
           <p className="pb-1">
-            Hello. My name is Ella. Welcome to my humble internet home. There
+            <span
+              onClick={() =>
+                setHello(hellos[Math.floor(Math.random() * hellos.length)])
+              }
+              className="animate-cursor-party"
+            >
+              {hello}
+            </span>{" "}
+            My name is Ella. Welcome to my humble internet home. There
             isn&apos;t much to do here.
           </p>
           <p className="pb-3">
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,76 @@ module.exports = {
         white: "#FFFEFD",
         black: "#181818",
       },
+      keyframes: {
+        "cursor-party": {
+          "0%": {
+            cursor: "grabbing",
+          },
+          "5%": {
+            cursor: "grab",
+          },
+          "10%": {
+            cursor: "zoom-out",
+          },
+          "15%": {
+            cursor: "zoom-in",
+          },
+          "20%": {
+            cursor: "all-scroll",
+          },
+          "25%": {
+            cursor: "row-resize",
+          },
+          "30%": {
+            cursor: "zoom-in",
+          },
+          "35%": {
+            cursor: "text",
+          },
+          "40%": {
+            cursor: "crosshair",
+          },
+          "45%": {
+            cursor: "progress",
+          },
+          "50%": {
+            cursor: "pointer",
+          },
+          "55%": {
+            cursor: "context-menu",
+          },
+          "60%": {
+            cursor: "none",
+          },
+          "65%": {
+            cursor: "help",
+          },
+          "70%": {
+            cursor: "vertical-text",
+          },
+          "75%": {
+            cursor: "alias",
+          },
+          "80%": {
+            cursor: "copy",
+          },
+          "85%": {
+            cursor: "move",
+          },
+          "90%": {
+            cursor: "no-drop",
+          },
+          "95%": {
+            cursor: "pointer",
+          },
+          "100%": {
+            cursor: "ew-resize",
+          },
+        },
+      },
+      animation: {
+        "cursor-party": "cursor-party 3s infinite ease-in-out",
+      },
     },
   },
   variants: {
