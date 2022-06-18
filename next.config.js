99417325ceb62dfc894eaaf74f29a81592834739
Ella <git@ella.cx>
2022-06-18 11:09:30 -0400

    Set up MDX and fix import error?

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
+module.exports = withMDX({
+  // Append the default value with md extensions
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+});
+
 module.exports = {
   typescript: {
     // !! WARN !!
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import Link from "next/link";
 import Image from "next/image";
 import cat from "../public/images/cat.jpg";
